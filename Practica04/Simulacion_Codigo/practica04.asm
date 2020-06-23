70770acf976441927f4d44b52a27681fcb6ed98f
Zait06 <zait-hdez@hotmail.com>
2020-06-23 08:58:41 -0500

    Practica 4

--- Practica04/Simulacion_Codigo/practica04.asm
+++ Practica04/Simulacion_Codigo/practica04.asm
@@ -22,57 +22,53 @@ main:
 	sei
 	
 load:
-	ldi R20, $3F
-	ldi R21, $06
-	ldi R22, $5B
-	ldi R23, $4F
-	ldi R24, $66
-	ldi R25, $6D
-	ldi R26, $7D
-	ldi R27, $27
-	ldi R28, $7F
-	ldi R29, $6F
+	ldi R20, $3F	; 0
+	ldi R21, $06	; 1
+	ldi R22, $5B	; 2
+	ldi R23, $4F	; 3
+	ldi R24, $66	; 4
+	ldi R25, $6D	; 5
+	ldi R26, $7D	; 6
+	ldi R27, $27	; 7
+	ldi R28, $7F	; 8
+	ldi R29, $6F	; 9
 	clr unidades
 	clr decenas
 	clr ZH
 	
 loop:
 	rcall BCD_unidades
-	out PORTC, aux
+	out PORTC, aux		; Ponemos el valor de unidades en el puerto C
 	rcall BCD_decenas
-	out PORTA, aux
+	out PORTA, aux		; Ponemos el valor de decenas en el puerto A
 	rjmp loop
 	
-BCD_unidades:
+BCD_unidades:			; Le damos a las unidades el valor BCD
 	ldi ZL, 20
 	add ZL, unidades
 	ld aux, Z
 	ret
 	
-BCD_decenas:
+BCD_decenas:			; Le damos a las decenas el valor BCD
 	ldi ZL, 20
 	add ZL, decenas
 	ld aux, Z
 	ret
 	
-aumen:
-	inc unidades
+aumen:					; Se acciona el contador cada vez que se acabe un ciclo
+	inc unidades		; Se incrementa en uno unidades
 	cpi unidades, 10
 	breq a_dece
 	reti
 	
-a_dece:
-	clr unidades
-	inc decenas
+a_dece:					; Si las unidades es igual a 10
+	clr unidades		; Se pone unidades en cero	
+	inc decenas			; Se incrementa en uno decenas
 	cpi decenas, 10
 	breq restart
 	reti
 	
-restart:
-	clr unidades
-	clr decenas
-	reti
-	
-maxi:
-	ldi decenas, 9
+restart:				; Si decenas es igual a diez
+	clr unidades		; Se inicia el contador en cero
+	clr decenas			; Es decir, se reinicia todo
 	reti
